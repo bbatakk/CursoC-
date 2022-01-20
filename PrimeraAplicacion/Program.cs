0dc32508e4f75bd5167e174b7e052cad6b326c5c
Àlex Abad Rodriguez <alexabad08@gmail.com>
2022-01-20 19:40:36 +0100

    initial commit

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un codigo");
+
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce segundo codigo");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            int numTotal = num1 + num2;
+
+            Console.WriteLine($"La suma és: {numTotal}");
+            //holaqeta
+             
+        }
+    }
+}
+ 
